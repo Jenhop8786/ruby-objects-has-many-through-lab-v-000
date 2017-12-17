09e4455d389296bbef202e0363c5ea016d2bc437
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-17 00:22:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
-class Song 
+class Song
   attr_accessor :name, :genre, :artist
 
 
   def initialize(name, genre)
-    @name = name 
-    @genre = genre 
+    @name = name
+    @genre = genre
     genre.add_song(self)
-  end 
+  end
 
-end 
+end
